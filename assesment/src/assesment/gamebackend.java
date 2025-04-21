d9ac21ac066e993c58e2aa2e210f35b0aa793241
Pratham B S <prathamprathupks@gmail.com>
2025-04-21 19:37:49 +0530

    Add files via upload

---
+++ assesment/src/assesment/gamebackend.java
@@ -0,0 +1,30 @@
+package assesment;
+import java.util.Scanner;
+public class gamebackend {
+public static void main(String[] args) {
+	gamestore op =new gamestore();
+	Scanner sc =new Scanner(System.in);
+	op.gamestore();
+	System.out.println("enter the choice");
+	int res=sc.nextInt();
+	op.operation(0, res);
+    int copies=sc.nextInt();
+	if(res==1) {
+		System.out.println(1500*(copies));
+	}if(res==2){
+		System.out.println(1200*(copies));
+	}if(res==3) {
+		System.out.println(1000*(copies));
+	}if(res==4) {
+		System.out.println(800*copies);
+	}if(res==5) {
+		System.out.println(500*copies);
+	}else{
+		System.out.println("thank you");
+	}
+	
+}
+
+
+ }
+ 
